90c57c1c791be9083437936cd373c2fbfccdc5d6
NginaKamau <loisek.ngina@strathmore.edu>
2025-04-01 21:39:13 +0300

    Added two loops that will collect the student details and the scores and align them with the student and IDs Also some changes done to the arrays that collect the scores,IDs and the names of the students

--- StudentsScores.java
+++ StudentsScores.java
@@ -3,8 +3,25 @@ public class StudentsScores {
     public static void main(String[]args){
         Scanner scanner=new Scanner(System.in);
 
-        String[][] students=new String[10][5];
-        int scores[][]=new int[10][5];
+        //Arrays to make it easier to store the students and scores
+        String[] students=new String[10];
+        int[][] scores=new int[10][5];
+        String[] ids=new String[10];
+
+        //For statement that collect student details and scores
+        for (int i=0; i<10; i++){
+            System.out.print("Enter name for student"+(i+1)+":");
+            students[i]=scanner.nextLine();
+            System.out.print("Enter ID for student"+(i+1)+":");
+            ids[i]=scanner.nextLine();
+
+          //Loops to collect scores for each subject
+          for(int j=0; j<5; j++) {
+              System.out.print("Enter score for subject"+(j+1)+":");
+              scores[i][j]=scanner.nextInt();
+          }
+
+        }
     }
 
 }
