75d5edbb4804bae3215fa9c8d30fb0b164eda4fe
NginaKamau <loisek.ngina@strathmore.edu>
2025-04-01 21:15:10 +0300

    Creating the class and the arrays that will store the students scores and the students themselves

---
+++ StudentsScores.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class StudentsScores {
+    public static void main(String[]args){
+        Scanner scanner=new Scanner(System.in);
+
+        String[][] students=new String[10][5];
+        int scores[][]=new int[10][5];
+    }
+
+}
