d7f868af4c6bac3c0a8ed83e661683e398e92dc6
NginaKamau <loisek.ngina@strathmore.edu>
2025-04-01 21:57:33 +0300

    Did changes to the code to include the methods and some changes to the names of the variables as well

---
+++ StudentsDetails.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StudentsDetails {
+    public static void main(String[]args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Arrays to make it easier to store the students and scores
+        String[] studentsNames = new String[10];
+        int[][] studentsScores = new int[10][5];
+        String[] studentsIds = new String[10];
+
+        //Invoking the methods
+        gatherStudentInfo(studentsNames,studentsIds,scanner);
+        collectScores(studentsScores,scanner);
+        showStudentInfo(studentsScores,studentsIds,studentsScores);
+
+
+    }
+}
