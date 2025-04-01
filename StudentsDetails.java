bd20852cca977a3206714cd62c3f280a44530902
NginaKamau <loisek.ngina@strathmore.edu>
2025-04-01 22:01:55 +0300

    Created a method to collect student names and IDs

--- StudentsDetails.java
+++ StudentsDetails.java
@@ -16,4 +16,14 @@ public class StudentsDetails {
 
 
     }
+
+    //Method to collect student names and IDs
+    public static void gatherStudentInfo(String[]names,String[]ids,Scanner input){
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Enter the name of student " + (i + 1) + ": ");
+            names[i] = input.nextLine();
+            System.out.print("Enter the ID of student " + (i + 1) + ": ");
+            ids[i] = input.nextLine();
+        }
+    }
 }
